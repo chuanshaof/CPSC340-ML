fd5bd31dfbee2e7f8857d89a9a5428ccf92cf4bb
chuanshaof <chuanshaof@gmail.com>
2022-11-27 12:28:14 -0800

    most done with some questions left out

--- assignment5/a5/example_PCA.jl
+++ assignment5/a5/example_PCA.jl
@@ -5,6 +5,17 @@ dataTable = readdlm("animals.csv",',')
 X = float(real(dataTable[2:end,2:end]))
 (n,d) = size(X)
 
+include("PCA.jl")
+model = PCA(X, 2)
+Z = model.compress(X)
+
 # Plot matrix as image
 using Plots
-heatmap(X)
+graph = scatter(Z[:,1], Z[:,2], label="PCA", legend=:topleft)
+
+for i in 1:n
+    annotate!(Z[i,1],Z[i,2],dataTable[i+1,1],annotationfontsize=8)
+end
+
+plot(graph)
+    
